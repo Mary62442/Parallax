e9c8ba4addfe02b4795af1b616d133372089d56a
Maria Burlando <maria@dmm888.com>
2017-10-29 12:02:24 +0100

    Installed remaining gulp packages, created a new navbar responsive to the scrolling direction, fixed margins and paddings

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+﻿let gulp = require('gulp');
+let sass = require('gulp-sass');
+let webserver = require('gulp-webserver');
+let uglify = require('gulp-uglify');
+let uglifycss = require('gulp-uglifycss');
+let babel = require('gulp-babel');
+
+//In order for tasks to work in sequence
+//make sure the tasks return a stream or promise, or handle the callback
+
+gulp.task('uglifycss', ['sass'], ()=> {
+  return  gulp.src('css/**/*.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// The task that depends on uglifycss task and must be executed first
+gulp.task('sass', ()=> {
+      return   gulp.src('scss/**/*.scss')
+        .pipe(sass()) 
+        .pipe(gulp.dest('css'));   
+});
+
+
+gulp.task('watch', ()=> {
+    gulp.watch('scss/**/*.scss', ['uglifycss']);
+    gulp.watch('scripts/**/*.js', ['compressjs']);
+});
+ 
+gulp.task('webserver', ['uglifycss', 'compressjs'], ()=> {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+// Javascript minification task
+gulp.task('compressjs', (cb) => {
+  return gulp.src(['scripts/*.js'])
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+    cb   
+});
+
+
+gulp.task('default', ['webserver','watch']);
+
---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+﻿let win = $(window);
+let lastY = win.scrollTop();
+
+win.scroll(() => {
+
+    let scrolled = win.scrollTop();
+    let firstSec = document.getElementById("first-sec");
+    let firstSecScroll = firstSec.scrollHeight;
+
+    if (scrolled < firstSecScroll) {
+     	$('.background').css('z-index', '-1');
+ 	}
+    else {    
+     	$('.background').css('z-index', '-3');
+	}
+
+    $('.background').css('top', -(scrolled * 0.1) + 'px');
+    $('.background1').css('top', -(scrolled * 0.1) + 'px');
+
+    let y = (scrolled > lastY) ? 'down' :((scrolled===lastY)? 'none':'up');
+    
+
+	if (y === 'down') {
+		$('#navbar').slideUp(300);
+	}
+		
+    else if (y === 'up') {
+    	$('#navbar').slideDown(300);
+    }
+    
+    lastY = scrolled; 
+    
+});
+
+
